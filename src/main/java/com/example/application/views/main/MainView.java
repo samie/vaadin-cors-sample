67f32fe8ba1367e40751a277ca143c732fc869ae
Sami Ekblad <sami@vaadin.com>
2024-07-25 13:08:16 +0300

    Implemented newsletter subscription

--- src/main/java/com/example/application/views/main/MainView.java
+++ src/main/java/com/example/application/views/main/MainView.java
@@ -1,35 +1,89 @@
 package com.example.application.views.main;
 
-import com.vaadin.flow.component.html.H2;
-import com.vaadin.flow.component.html.Image;
-import com.vaadin.flow.component.html.Paragraph;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.Text;
+import com.vaadin.flow.component.WebComponentExporter;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.webcomponent.WebComponent;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import com.vaadin.flow.router.RouteAlias;
-import com.vaadin.flow.theme.lumo.LumoUtility.Margin;
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
-@PageTitle("Main")
+import java.io.IOException;
+
+@PageTitle("Vaadin CORS sample")
 @Route(value = "")
-@RouteAlias(value = "")
-public class MainView extends VerticalLayout {
+public class MainView extends Div {
+
+    public static final String TAG = "newsletter-subscription";
 
     public MainView() {
-        setSpacing(false);
 
-        Image img = new Image("images/empty-plant.png", "placeholder plant");
-        img.setWidth("200px");
-        add(img);
+//        HorizontalLayout layout = new HorizontalLayout(FlexComponent.Alignment.BASELINE);
+        TextField emailField = new TextField("Email");
+        Button subscribeButton = new Button("Subscribe");
+        Text thankYouLabel = new Text("");
 
-        H2 header = new H2("This place intentionally left empty");
-        header.addClassNames(Margin.Top.XLARGE, Margin.Bottom.MEDIUM);
-        add(header);
-        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
-
-        setSizeFull();
-        setJustifyContentMode(JustifyContentMode.CENTER);
-        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
-        getStyle().set("text-align", "center");
+        subscribeButton.addClickListener(click -> {
+            thankYouLabel.setText("Thank you for subscribing!");
+            emailField.setVisible(false);
+            subscribeButton.setVisible(false);
+        });
+        HorizontalLayout layout = new HorizontalLayout(FlexComponent.Alignment.BASELINE, emailField, subscribeButton, thankYouLabel);
+        layout.setWidth("100%");
+        layout.setPadding(true);
+        emailField.setWidth("80%");
+        subscribeButton.setWidth("20%");
+        add(layout);
     }
 
+    public static class NewsletterSubscriptionExporter extends WebComponentExporter<MainView> {
+        public NewsletterSubscriptionExporter() {
+            super(TAG);
+        }
+
+        @Override
+        protected void configureInstance(WebComponent<MainView> webComponent, MainView mainView) {
+            // Nothing to configure
+        }
+    }
+
+    @WebFilter(filterName = "CORS Filter", asyncSupported = true, value = "/*")
+    public static class CORSFilter implements Filter {
+        @Override
+        public void init(FilterConfig filterConfig) throws ServletException {
+        }
+
+        @Override
+        public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+            HttpServletRequest request = (HttpServletRequest) servletRequest;
+            HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+            String origin = request.getHeader("Origin");
+            if (isOrginAllowed(origin)) {
+                response.setHeader("Access-Control-Allow-Origin", "*");
+                response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+                response.setHeader("Access-Control-Allow-Headers", "Content-Type");
+            }
+
+            if ("options".equalsIgnoreCase(request.getMethod())) {
+                response.addHeader("Access-Control-Allow-Methods", "GET, POST");
+                response.addHeader("Access-Control-Allow-Headers", "content-type");
+                response.getWriter().flush();
+                return;
+            }
+            filterChain.doFilter(servletRequest, servletResponse);
+
+        }
+
+        private boolean isOrginAllowed(String origin) {
+            return true;
+        }
+    }
 }
