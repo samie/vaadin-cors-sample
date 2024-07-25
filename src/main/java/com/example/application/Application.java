95b36862690884b5c0618adadca424f0eead4f16
Sami Ekblad <sami@vaadin.com>
2024-07-25 16:30:22 +0300

    Removed theme

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -13,7 +13,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication
-@Theme(value = "my-app")
 public class Application implements AppShellConfigurator {
 
     public static void main(String[] args) {
